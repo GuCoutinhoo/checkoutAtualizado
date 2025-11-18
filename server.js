3a54f5e084f1da9076e182807613b2e63e13ce32
GuCoutinhoo <gustavohcsantos.mm2020@gmail.com>
2025-11-18 11:23:33 -0300

    Refactor server.js for improved clarity

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Configurar o Access Token do Mercado Pago (NUNCA coloque no front)
+// Configurar Access Token (NUNCA colocar no front-end)
 const mp = new MercadoPago({
   accessToken: "APP_USR-5079596909849619-111808-bb8409b6a366cbaadb074c81de680f9a-521005600"
 });
@@ -32,7 +32,6 @@ app.post("/create_preference", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+app.listen(3001, () => {
+  console.log("Servidor rodando em http://localhost:3001");
 });
